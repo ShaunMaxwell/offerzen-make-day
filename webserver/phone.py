20de7d535fac2d571cc6e61689b24222d0a25419
Nicky Reid <nichola.reid@takealot.com>
2018-03-22 14:23:24 +0200

    werk werk werk

--- webserver/phone.py
+++ webserver/phone.py
@@ -5,10 +5,33 @@ class Phone():
 
     def __init__(self):
         self.apikey = PhonealotConfig.DF_API_KEY
+        # self.tal_qry = "https://api.takealot.com/rest/v-1-6-0/productlines/search?sort=Default%20Descending&rows=1&start=0&detail=mlisting&qsearch={0}&filter=Available:true"
+        self.tal_qry = "https://api.takealot.com/rest/v-1-6-0/productlines/search?sort=Default%20Descending&rows=10&start=0&detail=mlisting&qsearch={0}&filter=Type:16&Available:true"
+
+    def get_all_models(self):
+        return requests.get('https://sandbox.root.co.za/v1/insurance/modules/root_gadgets/models',
+                                auth=(self.apikey, ''))
 
     def get_brands(self):
-        response = requests.get('https://sandbox.root.co.za/v1/insurance/modules/root_gadgets/models',
-                                auth=(self.apikey, ''))
-        models = response.json()
-        brands = set([phone['make'] for phone in models])
+        all_models = self.get_all_models().json()
+        brands = set([phone['make'].encode('ascii') for phone in all_models])
         return list(brands)
+
+    def get_models_by_brand(self, brand=None):
+        all_models = self.get_all_models().json()
+        if brand:
+            models = list(set([phone['name'].encode('ascii') for phone in all_models if brand.lower() in phone['make'].lower()]))
+        else:
+            models = None
+        return_dict = {}
+        for (idx, model) in enumerate(models):
+            return_dict[idx] = model
+        return return_dict
+
+    def get_plid(self, brand, phone):
+        search = self.tal_qry.format(brand + "%20" + phone)
+        print search
+        result = requests.get(search).json().get('results')
+        return "https://www.takealot.com/title/{0}".format(result.get('productlines')[0].get('id'))
+
+
--- webserver/server.py
+++ webserver/server.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 logger = logging.getLogger(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def hello():
-    return "Hello World!"
+    function = request.get_json().get("result").get("action").lower()
+    if "brand" in function:
+        return get_brands()
+    elif "model" in function:
+        return get_models()
+    elif "pdp" in function:
+        return get_plid()
+    else:
+        response = app.response_class(
+        response=json.dumps("hello world"),
+        status=200,
+        mimetype='application/json')
+        return response
 
 
 @app.route("/make_name", methods=['POST'])
@@ -28,15 +40,70 @@ def make_name():
     )
     return response
 
-@app.route("/get_phones", methods=['GET'])
+@app.route("/get_brands", methods=['POST'])
 def get_brands():
     """
     gets list of phones from root API
     :return: list of supported phone models
     """
     phone = Phone()
+    brands = phone.get_brands()
+    text = "Sure! Here are the available brands:\n " \
+           "{0}\n" \
+           "What do you want?".format(brands)
+
+
+    result = {'speech': text, 'displayText': text}
+    # print result
+
     response = app.response_class(
-        response=json.dumps(phone.get_brands()),
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/get_models", methods=['POST'])
+def get_models():
+    parameters = request.get_json().get("result").get("parameters")
+    brand = parameters.get('brand')
+    # print brand
+    phone = Phone()
+    models = phone.get_models_by_brand(brand)
+    text = "Here are the available models for {0}:\n{1}".format(brand,models)
+    result = {'speech': text, 'displayText': text}
+    # print result
+    response = app.response_class(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/get_plid", methods=['POST'])
+def get_plid():
+    p = Phone()
+    parameters = request.get_json().get("result").get("parameters")
+    print request.get_json().get("result").get('contexts')
+    try:
+        brand = request.get_json().get("result").get('contexts')[3].get("parameters").get("brand")
+    except:
+        brand = "Samsung"
+
+    print "selected brand = {0}".format(brand)
+    model_number = parameters.get('number', 0)
+    if type(model_number) != int :
+        model_number = int(parameters.get('number', 0).encode('ascii'))
+
+    models = p.get_models_by_brand(brand)
+
+    selected = models[model_number]
+    print selected
+    plid = p.get_plid(brand, selected)
+    print plid
+    result = {'speech': plid, 'displayText': plid}
+    response = app.response_class(
+        response=json.dumps(result),
         status=200,
         mimetype='application/json'
     )
